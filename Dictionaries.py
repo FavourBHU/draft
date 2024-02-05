19befd4438bc21c0ac9707d3eb7e57e977de26d9
FavourBHU <150298243+FavourBHU@users.noreply.github.com>
2024-02-05 23:53:48 +0100

    amj mxa
    nax x

---
+++ Dictionaries.py
@@ -0,0 +1,13 @@
+igbo = {
+    'come':'bia',
+    'go':'puo',
+    'eat':'rie',
+    'road': 'uzo',
+    'plate': 'efere',
+    'chukwu': 'God',
+    'name': 'afa',
+     'father':'mpa',
+     'mother':'mma'
+}
+word = input('Search for a word')
+print(igbo[word])
